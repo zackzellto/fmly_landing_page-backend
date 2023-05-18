8cb63161bb205b256bd5ff2d0ce29959eeefea4c
Zack Zellto <zacharyzellto@gmail.com>
2023-05-17 18:45:15 -0600

    allowing all origins

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 
-CORS(app, origins=['https://fmly-landing-page.herokuapp.com/api/waitlist'], allow_headers=[
+CORS(app, origins=['*'], allow_headers=[
      "Content-Type", "Authorization", "Access-Control-Allow-Credentials"], supports_credentials=True)
 
 SWAGGER_URL = '/swagger'
